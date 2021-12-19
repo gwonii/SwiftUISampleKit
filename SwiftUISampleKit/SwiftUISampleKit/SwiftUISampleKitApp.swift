45eb1dec0575c44caf4304b9c97c9304f160b8cc
gwnoii <hc.gwonii@gmail.com>
2021-12-19 14:19:24 +0900

    init: SwiftUI 초기 프로젝트 생성

---
+++ SwiftUISampleKit/SwiftUISampleKit/SwiftUISampleKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUISampleKitApp.swift
+//  SwiftUISampleKit
+//
+//  Created by Hogwon CHOI on 2021/12/19.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUISampleKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
